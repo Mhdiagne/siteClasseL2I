9ccc87191dfd9c03a1a91dd4038afd54a0a51fa2
gaye-00 <gayeabdoulaye163@gmail.com>
2024-06-12 16:58:21 +0000

    first commit to Back_End

---
+++ src/main/java/com/l2i/siteL2I/SiteL2IApplication.java
@@ -0,0 +1,13 @@
+package com.l2i.siteL2I;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteL2IApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteL2IApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/l2i/siteL2I/SiteL2IApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l2i.siteL2I;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteL2IApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
